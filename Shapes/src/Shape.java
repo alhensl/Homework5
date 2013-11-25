de4cde54f539c2fc2fe752f8878dc79d3cd48306
Adam Hensley <adam.hensley@asu.edu>
2013-11-25 12:26:47 -0700

    version 1

--- Shapes/src/Shape.java
+++ Shapes/src/Shape.java
@@ -1,4 +1,20 @@
 
-public class Shape {
- 
+public class Shape
+{
+  protected double area;
+  protected double perimeter;
+
+ public Shape()
+ {
+   area=perimeter=0;
+ }
+  public String toString()
+  	 {
+
+  	     return "area = " + area +
+                  " \t perimeter = " + perimeter;
+
+  	 }
+
+
 }
